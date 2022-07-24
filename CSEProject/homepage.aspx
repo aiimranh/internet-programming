a943eed8b1d97b042629acc1ea4455452072bf05
Md. Imran <hmimran.in@gmail.com>
2022-07-24 19:15:32 +0600

    final project

---
+++ CSEProject/homepage.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="homepage.aspx.cs" Inherits="CSEProject.homepage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+   <section>
+   <div class="bd-example">
+   <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+   <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="imgs/homepage2.jpg" class="d-block w-100" alt="homepage2.jpg">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Reading Room</h5>
+          <span style="color:black; background-color: yellow;">"I have always imagined that Paradise will be a kind of a Library.".</span>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="imgs/homepage2.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Reading Room</h5>
+          <span style="color:black; background-color: yellow;">"I have always imagined that Paradise will be a kind of a Library.".</span>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="imgs/homepage2.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Reading Room</h5>
+          <span style="color:black; background-color: yellow;">"I have always imagined that Paradise will be a kind of a Library.".</span>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+</section>
+
+</asp:Content>
---
+++ CSEProject/userlogin.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CSEProject
+{
+    public partial class userlogin : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Request.Cookies["member_id"] != null)
+                {
+                    TextBox1.Text = Request.Cookies["member_id"].Value.ToString();
+                }
+                if (Request.Cookies["password"] != null)
+                {
+                    TextBox2.Text = Request.Cookies["password"].Value.ToString();
+                }
+                if (Request.Cookies["member_id"] != null && Request.Cookies["password"] != null)
+                {
+                    CheckBox1.Checked = true;
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+
+                }
+                SqlCommand cmd = new SqlCommand("select * from member_master_tbl where member_id='" + TextBox1.Text.Trim() + "' AND password='" + TextBox2.Text.Trim() + "'", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    if(CheckBox1.Checked == true)
+                    {
+                        Response.Cookies["member_id"].Value = TextBox1.Text;
+                        Response.Cookies["password"].Value = TextBox2.Text;
+                        Response.Cookies["member_id"].Expires = DateTime.Now.AddDays(2);
+                        Response.Cookies["password"].Expires = DateTime.Now.AddDays(2);
+                    }
+                    else
+                    {
+                        Response.Cookies["member_id"].Expires = DateTime.Now.AddDays(-1);
+                        Response.Cookies["password"].Expires = DateTime.Now.AddDays(-1);
+                    }
+
+                    while (dr.Read())
+                    {
+                        Response.Write("<script>alert('Login Successful');</script>");
+                        Session["username"] = dr.GetValue(8).ToString();
+                        Session["fullname"] = dr.GetValue(0).ToString();
+                        Session["role"] = "user";
+                        Session["status"] = dr.GetValue(10).ToString();
+                    }
+                    Response.Redirect("homepage.aspx");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid credentials');</script>");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+    }
+}
